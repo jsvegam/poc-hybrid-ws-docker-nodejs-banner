c67fc519c9c241e4ef120d70b296e23a8659b5d4
José Vega Marivil <Jose.Vega@uss.cl>
2021-11-04 00:43:47 -0300

    change db config

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,10 @@
+// module.exports = {
+//     user : "WS_PROCESOSACADEMICOS",
+//     password: "web,,159",
+//     connectString : "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = ussdtcodaora01-vip.uss.cl)(PORT = 1521))(CONNECT_DATA =(SERVICE_NAME = PROD.USS.CL)))"
+// };
 module.exports = {
     user : "WS_PROCESOSACADEMICOS",
     password: "web,,159",
-    connectString : "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = ussdtcodaora01-vip.uss.cl)(PORT = 1521))(CONNECT_DATA =(SERVICE_NAME = PROD.USS.CL)))"
+    connectString : "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 10.100.151.52)(PORT = 1521))(CONNECT_DATA =(SERVICE_NAME = PROD.USS.CL)))"
 };
